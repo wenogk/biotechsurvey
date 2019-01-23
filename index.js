f4a78dd4b975a6e60e6fb65e4fbfb6905b770d80
Romeno <wenogk@nyu.edu>
2019-01-23 20:27:47 +0400

    Update index.js

--- index.js
+++ index.js
@@ -167,14 +167,13 @@ $(_=>{
     $("#resultHeader").text(survey.data.question1 + ", thank you for your participation in our survey.")
     if(survey.data.question4 == "item1"){
       $("#resultBody").text("Since you strongly disagree with the use of GMOs, we would like to show you some facts and try to show you our point of view on GMOs")
-      $("#resultBody").append("<img src='images/impact-of-biotech_0.png' class='img-fluid' /><br /> ")
-      $("#resultBody").append("Watch this video that weighs in on whether GMOs are good or bad:<br /><div class='embed-responsive embed-responsive-16by9'><iframe class='embed-responsive-item' src='https://www.youtube.com/embed/7TmcXYp8xu4?controls=0&rel=0' allowfullscreen></iframe></div><br/>");
-    }
+      $("#resultBody").append("<img src='images/impact-of-biotech_0.png' class='img-fluid' /><br /><br />")
+      $("#resultBody").append("Watch this video that weighs in on whether GMOs are good or bad:<br /><br /><div class='embed-responsive embed-responsive-16by9'><iframe class='embed-responsive-item' src='https://www.youtube.com/embed/7TmcXYp8xu4?controls=0&rel=0' allowfullscreen></iframe></div><br/><br />");
+   }
     if(survey.data.question4 == "item2") {
       $("#resultBody").text("Well that's a smart position to be in, let's look at some statistics of GMO's to see if we can convince you that GMOs are indeed good for us. ")
       $("#resultBody").append("<img src='images/impact-of-biotech_0.png' class='img-fluid' /><br /><br />")
       $("#resultBody").append("Watch this video that weighs in on whether GMOs are good or bad:<br /><br /><div class='embed-responsive embed-responsive-16by9'><iframe class='embed-responsive-item' src='https://www.youtube.com/embed/7TmcXYp8xu4?controls=0&rel=0' allowfullscreen></iframe></div><br/><br />");
-   
     }
     if(survey.data.question4 == "item3"){
       $("#resultBody").text("Great! You support GMO's too, here's a cookie for you: ")
