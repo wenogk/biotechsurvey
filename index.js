652a6994f5ed8d00f3d96533859a2694906cb863
Romeno <wenogk@nyu.edu>
2019-01-23 15:11:50 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,155 @@
 $(_=>{
 
   Survey.StylesManager.applyTheme('bootstrap')
-
-
-  var surveyJSON = {pages:[{name:"page1",elements:[{type:"text",name:"question1",title:"Name"},{type:"dropdown",name:"question2",title:"Age",choices:[{value:"item1",text:"5-13"},{value:"item2",text:"13-18"},{value:"item3",text:"18-25"},{value:"item4",text:"25-35"},{value:"item5",text:"35-45"},{value:"item6",text:"45-55"},{value:"item7",text:"55-65"},{value:"item8",text:"65+"},"item9"]},{type:"dropdown",name:"question3",title:"What part of the world do you come from",choices:[{value:"item1",text:"Asia"},{value:"item2",text:"Africa"},{value:"item3",text:"Australia"},{value:"item4",text:"North America"},{value:"item5",text:"South America"},{value:"item6",text:"Europe"},{value:"item7",text:"Antarctica"}]}]},{name:"page2",elements:[{type:"radiogroup",name:"question4",title:"Do you support GMO's",choices:[{value:"item1",text:"Not at all"},{value:"item2",text:"Maybe"},{value:"item3",text:"Strongly"}]},{type:"radiogroup",name:"question5",title:"Have you ever had a GMO produced product?",choices:[{value:"item1",text:"Yes"},{value:"item2",text:"No"}]}]},{name:"page3",elements:[{type:"imagepicker",name:"question6",choices:[{value:"1",imageLink:"images/g1.jpg"},{value:"2",imageLink:"images/g2.jpg"},{value:"3",imageLink:"images/g3.jpg"},{value:"4",imageLink:"images/g4.jpg"}]}]}]}
-
+var surveyJSON = {
+    pages: [{
+        name: "page1",
+        elements: [{
+            type: "text",
+            name: "question1",
+            title: "Name"
+        }, {
+            type: "dropdown",
+            name: "question2",
+            title: "Age",
+            choices: [{
+                value: "item1",
+                text: "5-13"
+            }, {
+                value: "item2",
+                text: "13-18"
+            }, {
+                value: "item3",
+                text: "18-25"
+            }, {
+                value: "item4",
+                text: "25-35"
+            }, {
+                value: "item5",
+                text: "35-45"
+            }, {
+                value: "item6",
+                text: "45-55"
+            }, {
+                value: "item7",
+                text: "55-65"
+            }, {
+                value: "item8",
+                text: "65+"
+            }, "item9"]
+        }, {
+            type: "dropdown",
+            name: "question3",
+            title: "What part of the world do you come from",
+            choices: [{
+                value: "item1",
+                text: "Asia"
+            }, {
+                value: "item2",
+                text: "Africa"
+            }, {
+                value: "item3",
+                text: "Australia"
+            }, {
+                value: "item4",
+                text: "North America"
+            }, {
+                value: "item5",
+                text: "South America"
+            }, {
+                value: "item6",
+                text: "Europe"
+            }, {
+                value: "item7",
+                text: "Antarctica"
+            }]
+        }]
+    }, {
+        name: "page2",
+        elements: [{
+            type: "radiogroup",
+            name: "question4",
+            title: "Do you support GMO's",
+            choices: [{
+                value: "item1",
+                text: "Not at all"
+            }, {
+                value: "item2",
+                text: "Maybe"
+            }, {
+                value: "item3",
+                text: "Strongly"
+            }]
+        }, {
+            type: "radiogroup",
+            name: "question5",
+            title: "Have you ever had a GMO produced product?",
+            choices: [{
+                value: "item1",
+                text: "Yes"
+            }, {
+                value: "item2",
+                text: "No"
+            }]
+        }]
+    }, {
+        name: "page3",
+        elements: [{
+            type: "imagepicker",
+            name: "question6",
+            choices: [{
+                value: "1",
+                imageLink: "images/g1.jpg"
+            }, {
+                value: "2",
+                imageLink: "images/g2.jpg"
+            }, {
+                value: "3",
+                imageLink: "images/g3.jpg"
+            }, {
+                value: "4",
+                imageLink: "images/g4.jpg"
+            }]
+        }]
+    }],
+    {
+        name: "page4",
+        elements: [{
+            type: "radiogroup",
+            name: "question7",
+            title: "Do you think there are health benefits associated with GMOs?",
+            choices: [{
+                value: "item1",
+                text: "Yes"
+            }, {
+                value: "item2",
+                text: "No"
+            }]
+        }, {
+            type: "radiogroup",
+            name: "question8",
+            title: "Do you think the regulations in place for GMOs are satisfactory?",
+            choices: [{
+                value: "item1",
+                text: "Yes"
+            }, {
+                value: "item2",
+                text: "No"
+            }]
+        }, {
+            type: "radiogroup",
+            name: "question9",
+            title: "Do you think GMOs and the companies that produce it support farmers?",
+            choices: [{
+                value: "item1",
+                text: "Yes"
+            }, {
+                value: "item2",
+                text: "No"
+            }]
+        }]
+    }]
+}
 
   let surveyResult = survey=>{
     $("#surveyContainer").remove()
