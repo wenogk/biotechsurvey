e3a22b91e3f63c1b6f9540f29fd42f8f83903723
Romeno <wenogk@nyu.edu>
2019-01-23 15:19:47 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 $(_=>{
 
   Survey.StylesManager.applyTheme('bootstrap')
-var surveyJSON = {
+  var surveyJSON = {
     pages: [{
         name: "page1",
         elements: [{
@@ -111,8 +111,44 @@ var surveyJSON = {
                 imageLink: "images/g4.jpg"
             }]
         }]
-    } ,{name:"page4",elements:[{type:"radiogroup",name:"question7",title:"Do you think there are health benefits associated with GMOs?",choices:[{value:"item1",text:"Yes"},{value:"item2",text:"No"}]},{type:"radiogroup",name:"question8",title:"Do you think the regulations in place for GMOs are satisfactory?",choices:[{value:"item1",text:"Yes"},{value:"item2",text:"No"}]},{type:"radiogroup",name:"question9",title:"Do you think GMOs and the companies that produce it support farmers?",choices:[{value:"item1",text:"Yes"},{value:"item2",text:"No"}]}]}]}
-
+    }, {
+        name: "page4",
+        elements: [{
+            type: "radiogroup",
+            name: "question7",
+            title: "Do you think there are health benefits associated with GMOs?",
+            choices: [{
+                value: "item1",
+                text: "Yes"
+            }, {
+                value: "item2",
+                text: "No"
+            }]
+        }, {
+            type: "radiogroup",
+            name: "question8",
+            title: "Do you think the regulations in place for GMOs are satisfactory?",
+            choices: [{
+                value: "item1",
+                text: "Yes"
+            }, {
+                value: "item2",
+                text: "No"
+            }]
+        }, {
+            type: "radiogroup",
+            name: "question9",
+            title: "Do you think GMOs and the companies that produce it support farmers?",
+            choices: [{
+                value: "item1",
+                text: "Yes"
+            }, {
+                value: "item2",
+                text: "No"
+            }]
+        }]
+    }]
+}
 
   let surveyResult = survey=>{
     $("#surveyContainer").remove()
